50826bada155b221180204e5dda6326a991e3447
Informatica <informatica@ITTS-6476.local>
2018-02-09 14:50:29 +0100

    DataStream a oggetti

--- src/Connection/ClientConnessioneTCP.java
+++ src/Connection/ClientConnessioneTCP.java
@@ -18,8 +18,8 @@ public class ClientConnessioneTCP {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args){
-        //oggetto da usare per realizzare la connessione TCP
+	
+	//oggetto da usare per realizzare la connessione TCP
         Socket connection = null;
         //nome o IP del server
         String serverAddress = "localhost";
@@ -28,21 +28,24 @@ public class ClientConnessioneTCP {
         
         DataOutputStream out;
         DataInputStream stream;
-        //apertura della connessione al server sulla porta specificata
-        try{
+		
+    public static void main(String[] args) throws IOException{
+        
+        ClientConnessioneTCP c = new ClientConnessioneTCP();
+        
+		c.apri();
+		c.comunica();
+		c.chiudi();
+        
+    
+	}
+	public void apri()throws IOException{
+		//apertura della connessione al server sulla porta specificata
+		try{
             connection = new Socket(serverAddress, port);
             System.out.println("Connessione aperta");
             
-			out = new DataOutputStream(connection.getOutputStream());
-            out.writeUTF("Dammi la data di oggi");
-            out.flush();
-            //out.close();
-            
-			System.out.print("Il Server risponde: ");
 			
-			stream=new DataInputStream(connection.getInputStream());
-			System.out.println(stream.readUTF());
-            //stream.close();
 					
         }
         catch(ConnectException e){
@@ -56,10 +59,23 @@ public class ClientConnessioneTCP {
             System.err.println(e2);
             e2.printStackTrace();
         }
-		
-        //chiusura della connnessione
-        finally{
-                try {
+	}
+	public void comunica() throws IOException{
+		out = new DataOutputStream(connection.getOutputStream());
+            out.writeUTF("Dammi la data di oggi");
+            out.flush();
+            //out.close();
+            
+			System.out.print("Il Server risponde: ");
+			
+			stream=new DataInputStream(connection.getInputStream());
+			System.out.println(stream.readUTF());
+            //stream.close();
+	}
+	
+	public void chiudi() throws IOException{
+		//chiusura della connnessione
+		try {
             if (connection!=null)
                 {
 					
@@ -70,6 +86,5 @@ public class ClientConnessioneTCP {
             catch(IOException e){
                 System.err.println("Errore nella chiusura della connessione!");
             }
-        }
-    }
+	}
 }
--- src/Connection/ServerConnessioneTCP.java
+++ src/Connection/ServerConnessioneTCP.java
@@ -17,59 +17,69 @@ import java.util.*;
  */
 public class ServerConnessioneTCP {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-        // porta del server maggiore di 1024 
-        int port = 2000;
-        //oggetto ServerSocket necessario per accettare richieste dal client
-        ServerSocket sSocket = null;
-        //oggetto da usare per realizzare la connessione TCP
-        Socket connection;
+	/**
+	 * @param args the command line arguments
+	 */
+	
+	
+	int port = 2000;
+	//oggetto ServerSocket necessario per accettare richieste dal client
+	ServerSocket sSocket = null;
+	//oggetto da usare per realizzare la connessione TCP
+	Socket connection;
 
-        DataOutputStream out;
-		DataInputStream stream;
-        DateFormat dateFormat;
-		String dataoggi;
-        while (true) {
-            try {
-                // il server si mette in ascolto sulla porta voluta
-                sSocket = new ServerSocket(port);
-                System.out.println("In attesa di connessioni!");
-                //si è stabilita la connessione
-                connection = sSocket.accept();
-                System.out.println("Connessione stabilita!");
-                System.out.println("Socket server: " + connection.getLocalSocketAddress());
-                System.out.println("Socket client: " + connection.getRemoteSocketAddress());
-                
-				System.out.print("Il Client dice: ");
-				
-				stream=new DataInputStream(connection.getInputStream());
-				System.out.println(stream.readUTF());
+	DataOutputStream out;
+	DataInputStream stream;
+	DateFormat dateFormat;
+	String dataoggi;
+
+	public static void main(String[] args) throws IOException {
+		ServerConnessioneTCP s=new ServerConnessioneTCP();
+		
+		s.attendi();
+		s.comunica();
+		s.chiudi();
+	}
+
+	public void attendi() throws IOException {
+		try {
+			sSocket = new ServerSocket(port);
+			System.out.println("In attesa di connessioni!");
+			//si è stabilita la connessione
+			connection = sSocket.accept();
+			System.out.println("Connessione stabilita!");
+			System.out.println("Socket server: " + connection.getLocalSocketAddress());
+			System.out.println("Socket client: " + connection.getRemoteSocketAddress());
+		} catch (IOException e) {
+			System.err.println("Errore di I/O!");
+		}
+
+	}
+
+	public void comunica() throws IOException {
+		System.out.print("Il Client dice: ");
+
+		stream = new DataInputStream(connection.getInputStream());
+		System.out.println(stream.readUTF());
 				//stream.close();
-				
-				dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-				Date date = new Date();
-                out = new DataOutputStream(connection.getOutputStream());
-				dataoggi=dateFormat.format(date);
-                out.writeUTF(dataoggi);
-                out.flush();
-                //out.close();
-            
-			} catch (IOException e) {
-                System.err.println("Errore di I/O!");
-            }
-            //chiusura della connessione con il client
-            try {
-                if (sSocket != null) {
-                    sSocket.close();
-                }
-            } catch (IOException ex) {
-                System.err.println("Errore nella chiusura della connessione!");
-            }
-            System.out.println("Connessione chiusa!");
-            
-        }
-    }
+
+		dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = new Date();
+		out = new DataOutputStream(connection.getOutputStream());
+		dataoggi = dateFormat.format(date);
+		out.writeUTF(dataoggi);
+		out.flush();
+		//out.close();
+	}
+
+	public void chiudi() throws IOException {
+		try {
+			if (sSocket != null) {
+				sSocket.close();
+			}
+		} catch (IOException ex) {
+			System.err.println("Errore nella chiusura della connessione!");
+		}
+		System.out.println("Connessione chiusa!");
+	}
 }
